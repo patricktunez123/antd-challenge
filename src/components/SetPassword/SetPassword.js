fc6b3e4485b6099d14e7c9981b7e804d41811e95
Patrick Tunezerwane <tunezepatrick@gmail.com>
2020-10-26 23:12:49 +0200

    ft(): login and register

---
+++ src/components/SetPassword/SetPassword.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SetPassword extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>This is the set password</div>
+         );
+    }
+}
+ 
+export default SetPassword;
